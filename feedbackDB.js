1fd65df0f7ad3cee3d2ac56d3dee3efc39055d87
TCGCWayPoint <riodanicaave02@gmail.com>
2025-04-15 10:45:18 +0800

    Initial Commit

--- feedbackDB.js
+++ feedbackDB.js
@@ -1,25 +1,15 @@
-// Import the mongoose library, which is used to interact with MongoDB in a Node.js application
 const mongoose = require('mongoose');
 
-// Establish a connection to the MongoDB database located at 'mongodb://localhost:27017/feedbackDB'
-// 'localhost:27017' is the default MongoDB host and port, and 'feedbackDB' is the database name
-// The options object specifies:
-// - useNewUrlParser: true - Enables the new URL parser to avoid deprecation warnings
-// - useUnifiedTopology: true - Enables the new connection topology engine for better stability
-mongoose.connect('mongodb://localhost:27017/feedbackDB', {
+// Replace the <...> placeholders with your actual Atlas credentials and DB name
+const uri = 'mongodb://riodanicaave02:gr@dSch00l@ac-3gomgnu-shard-00-00.syabp5w.mongodb.net:27017,ac-3gomgnu-shard-00-01.syabp5w.mongodb.net:27017,ac-3gomgnu-shard-00-02.syabp5w.mongodb.net:27017/?replicaSet=atlas-5u9h1m-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=feedbackDB';
+
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Store the mongoose connection object in a variable called 'db'
-// This object can be used to listen for connection events or perform other connection-related tasks
 const db = mongoose.connection;
 
-// Set up an event listener for the 'open' event on the connection
-// The 'open' event is triggered when the connection to MongoDB is successfully established
-// When triggered, it logs 'Connected to MongoDB' to the console to indicate a successful connection
-db.once('open', () => console.log('Connected to MongoDB'));
+db.once('open', () => console.log('Connected to MongoDB Atlas'));
 
-// Export the 'db' connection object so it can be imported and used in other files
-// This allows other modules to access the connection and listen for events or perform operations
 module.exports = db;
